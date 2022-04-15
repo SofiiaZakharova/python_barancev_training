46b7e496293560516581dd3b06178071b90c17a1
Sofiia_Zakharova <Sofiia_Zakharova@epam.com>
2022-04-15 17:05:12 +0300

    Добавлен тест для удаления группы, вынесена фикстура

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,5 @@
+
+
 class GroupHelper:
     def __init__(self, app):
         self.app = app
@@ -7,7 +9,7 @@ class GroupHelper:
         wd.find_element_by_link_text('groups').click()
 
     def create(self, group):
-        wd = self.wd
+        wd = self.app.wd
         self.open_groups_page()
         wd.find_element_by_name('new').click()
         wd.find_element_by_name('group_name').click()
@@ -21,6 +23,14 @@ class GroupHelper:
         wd.find_element_by_name('group_footer').send_keys(group.footer)
         wd.find_element_by_name('submit').click()
         self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name('selected[]').click()
+        wd.find_element_by_name('delete').click()
+        self.return_to_groups_page()
+
 
     def return_to_groups_page(self):
         wd = self.app.wd
